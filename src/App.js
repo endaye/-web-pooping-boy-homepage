a328dbf9661ec8aa13179c02015969cb138bbd48
Yuancheng Zhang <old.far.hot@gmail.com>
2022-07-25 09:05:52 +0800

    添加背景和logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className='App'>Poop Boi</div>;
+  return (
+    <div className='App'>
+      <img className='app-img-bg' src={process.env.PUBLIC_URL + "/img/bg.png"} alt='Background' />
+      <img className='app-img-logo' src={process.env.PUBLIC_URL + "/img/logo.svg"} alt='Poopboi Logo' />
+    </div>
+  );
 }
 
 export default App;
